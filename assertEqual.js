f4e952fb4a9f850d30a7ec1af9cf3a25b26192f9
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2020-08-18 03:43:46 +0000

    done

---
+++ assertEqual.js
@@ -0,0 +1,8 @@
+const assertEqual = (actual, expected) => {
+  return actual === expected
+    ? `✅✅✅ Assertion Passed: ${actual} === ${expected}`
+    : `🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`;
+};
+
+console.log(assertEqual('Lighthouse Labs', 'Bootcamp'));
+console.log(assertEqual(1, 1));
