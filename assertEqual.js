f15a8bac523f0f531c5198e114e0f245e8d5479d
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2020-08-18 13:17:48 +0000

    head function

--- assertEqual.js
+++ assertEqual.js
@@ -6,3 +6,10 @@ const assertEqual = (actual, expected) => {
 
 console.log(assertEqual('Lighthouse Labs', 'Bootcamp'));
 console.log(assertEqual(1, 1));
+
+const head = (array) => {
+  return array.length === 0 ? undefined : array.shift();
+};
+
+console.log(assertEqual(head([5, 6, 7]), 5));
+console.log(assertEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello'));
