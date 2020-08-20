797d1a75493b220bbc5a9e98b9f4c0a6c1f92792
Vladyslav <kytsevolov@gmail.com>
2020-08-20 14:12:32 -0600

    refactor

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -14,7 +14,6 @@ const eqObjects = function (object1, object2) {
 };
 
 const assertObjectsEqual = function (actual, expected) {
-  const inspect = require('util').inspect;
   return eqObjects(actual, expected)
     ? `✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`
     : `🛑🛑🛑Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`;
