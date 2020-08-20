af187a7bac1dde55b4c2636cd90925760ad22486
Vladyslav <kytsevolov@gmail.com>
2020-08-20 08:03:02 -0600

    bugs fixed

--- eqObjects.js
+++ eqObjects.js
@@ -4,11 +4,20 @@ const assertEqual = (actual, expected) => {
     : `🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`;
 };
 
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length === arr2.length) {
+    return arr1.every((val, i) => val === arr2[i]);
+  }
+  return false;
+};
+
 const eqObjects = function (object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) return false;
 
   for (let key in object1) {
-    if (object1[key] !== object2[key]) {
+    if (Array.isArray(object1[key])) {
+      return eqArrays(object1[key], object2[key]);
+    } else if (object1[key] !== object2[key]) {
       return false;
     }
   }
@@ -17,7 +26,14 @@ const eqObjects = function (object1, object2) {
 
 const ab = { a: '1', b: '2' };
 const ba = { b: '2', a: '1' };
-console.log(eqObjects(ab, ba)); // => true
+console.log(assertEqual(eqObjects(ab, ba), true)); // => true
 
 const abc = { a: '1', b: '2', c: '3' };
-console.log(eqObjects(ab, abc)); //
+console.log(assertEqual(eqObjects(ab, abc), false)); //
+
+const cd = { c: '1', d: ['2', 3] };
+const dc = { d: ['2', 3], c: '1' };
+console.log(assertEqual(eqObjects(cd, dc), true)); // => true
+
+const cd2 = { c: '1', d: ['2', 3, 4] };
+console.log(assertEqual(eqObjects(cd, cd2), false)); // => false
