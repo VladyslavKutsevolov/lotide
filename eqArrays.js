3fb22c546357d274d91f3e7ebf9fc0bde001f9fd
Vladyslav <kytsevolov@gmail.com>
2020-08-22 11:31:02 -0600

    check array recursively

--- eqArrays.js
+++ eqArrays.js
@@ -6,6 +6,25 @@ const assertEqual = (actual, expected) => {
 
 const eqArrays = (arr1, arr2) => {
   if (arr1.length === arr2.length) {
-    return arr1.every((val, i) => val === arr2[i]);
+    return arr1.every((val, i) => {
+      if (Array.isArray(val) || Array.isArray(arr2[i])) {
+        return eqArrays(val, arr2[i]) === false;
+      } else {
+        val === arr2[i];
+      }
+    });
   }
 };
+
+// console.log(eqArrays([[2, [3]], [4]], [[2, [3]], [4]])); // => true
+
+// console.log(
+//   eqArrays(
+//     [[2, 3], [4]],
+//     [
+//       [2, 3],
+//       [4, 5],
+//     ]
+//   )
+// ); // => false
+// console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])); // => false
