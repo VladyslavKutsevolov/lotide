35e2dea8e501c74eda15c7f3989dbb2526f7c5dc
Vladyslav <kytsevolov@gmail.com>
2020-08-22 11:31:24 -0600

    small fix

--- eqObjects.js
+++ eqObjects.js
@@ -16,7 +16,7 @@ const eqObjects = function (object1, object2) {
 
   for (let key in object1) {
     if (
-      object1[key].constructor === Object &&
+      object1[key].constructor === Object ||
       object2[key].constructor === Object
     ) {
       return eqObjects(object1[key], object2[key]);
@@ -33,23 +33,23 @@ const eqObjects = function (object1, object2) {
   return true;
 };
 
-const ab = { a: '1', b: '2' };
-const ba = { b: '2', a: '1' };
-console.log(assertEqual(eqObjects(ab, ba), true)); // => true
+// const ab = { a: '1', b: '2' };
+// const ba = { b: '2', a: '1' };
+// console.log(assertEqual(eqObjects(ab, ba), true)); // => true
 
-const abc = { a: '1', b: '2', c: '3' };
-console.log(assertEqual(eqObjects(ab, abc), false)); //
+// const abc = { a: '1', b: '2', c: '3' };
+// console.log(assertEqual(eqObjects(ab, abc), false)); //
 
-const cd = { c: '1', d: ['2', 3] };
-const dc = { d: ['2', 3], c: '1' };
-console.log(assertEqual(eqObjects(cd, dc), true)); // => true
+// const cd = { c: '1', d: ['2', 3] };
+// const dc = { d: ['2', 3], c: '1' };
+// console.log(assertEqual(eqObjects(cd, dc), true)); // => true
 
-const cd2 = { c: '1', d: ['2', 3, 4] };
-console.log(assertEqual(eqObjects(cd, cd2), false)); // => false
+// const cd2 = { c: '1', d: ['2', 3, 4] };
+// console.log(assertEqual(eqObjects(cd, cd2), false)); // => false
 
-console.log(
-  assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true)
-); // => true
+// console.log(
+//   assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true)
+// ); // => true
 
-console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => false
-console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 })); // => false
+// console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => false
+// console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 })); // => false
