184b914a387224db366b42423751c3473e84af4c
Vladyslav <kytsevolov@gmail.com>
2020-08-24 15:10:41 -0600

    middle test refactor

--- middle.js
+++ middle.js
@@ -1,23 +1,10 @@
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length === arr2.length) {
-    return arr1.every((val, i) => val === arr2[i]);
-  }
-};
-
-const assertArraysEqual = (arr1, arr2) => {
-  return eqArrays(arr1, arr2)
-    ? `✅✅✅ Assertion Passed`
-    : `🛑🛑🛑Assertion Failed`;
-};
-
 const middle = (array) => {
-  let middle = array[Math.round(array.length / 2)];
+  let middle = array[Math.floor(array.length / 2)];
   if (array.length > 2) {
     return array.length % 2 ? middle : (middle = [middle + 1, middle]);
   } else {
     return [];
   }
-  return middle;
 };
 
-console.log(middle([1, 5, 4, 3]));
+module.exports = middle;
---
+++ test/middle.test.js
@@ -0,0 +1,4 @@
+const middle = require('../middle');
+
+console.log(middle([1, 2, 3, 2, 6, 3, 3]));
+console.log(middle([1, 2, 5, 4, 3]));
