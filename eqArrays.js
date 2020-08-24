fa85312b75adbd70e9d9891f92fc81463f332163
Vladyslav <kytsevolov@gmail.com>
2020-08-24 14:50:08 -0600

    eq Array refactor

--- eqArrays.js
+++ eqArrays.js
@@ -1,9 +1,3 @@
-const assertEqual = (actual, expected) => {
-  return actual === expected
-    ? `✅✅✅ Assertion Passed: ${actual} === ${expected}`
-    : `🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`;
-};
-
 const eqArrays = (arr1, arr2) => {
   if (arr1.length === arr2.length) {
     return arr1.every((val, i) => {
@@ -16,15 +10,4 @@ const eqArrays = (arr1, arr2) => {
   }
 };
 
-// console.log(eqArrays([[2, [3]], [4]], [[2, [3]], [4]])); // => true
-
-// console.log(
-//   eqArrays(
-//     [[2, 3], [4]],
-//     [
-//       [2, 3],
-//       [4, 5],
-//     ]
-//   )
-// ); // => false
-// console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])); // => false
+module.exports = eqArrays;
